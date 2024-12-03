257c10545885a92d1d3959ed4938e5c3fc8039df
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2024-12-03 15:39:16 -0500

    Added Commend for Jack

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ def drug_interaction(request):
     drug1 = request.GET.get('drug1', '').strip()
     drug2 = request.GET.get('drug2', '').strip()
 
+    # Jack call you inference function here and get the interaction(yes/no) and interaction types(list of interaction)
     interaction= "No interaction"
     interaction_type = "No interaction"
     response_data = {
